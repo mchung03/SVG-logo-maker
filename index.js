8c86594940a4617f163f87ed1347c320b8cd59c1
mchung03 <minhee.chung14@gmail.com>
2023-07-28 15:33:26 -0700

    square class complete

---
+++ index.js
@@ -0,0 +1,59 @@
+const fs = require("fs")
+const inquirer = require('inquirer')
+const {SVG, Text, Square} = require('./lib/shapes.js')
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Type in a title for your image up to 3 characters',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'What color would you like your text to be?',
+            name: 'color'
+        },
+        {
+            type: 'list',
+            message: 'What shape would you like?',
+            name: 'shape',
+            choices: ['Circle', 'Triangle', 'Square']
+        },
+        {
+            type: 'input',
+            message: 'What color would you like your shape to be?',
+            name: 'fill'
+        },
+    ])
+    .then(ans => {
+        const myText = new Text(ans.color, ans.title)
+        let myShape;
+        if(ans.shape == "Square") {
+            myShape = new Square(ans.fill)
+        }
+        // else if other shapes'
+        const mySVG = new SVG(myShape, myText)
+        fs.writeFile("examples/hw.svg", mySVG.render(), err => {
+            if(err) {
+                console.log(err)
+            }
+        })
+    })
+
+
+// generate content of file
+    // generate svg tag
+    // generate svg shape
+    // generate text
+    // put all into one string
+
+
+
+// write file
+
+function createText(fillColor, text, textColor, shape) {
+    // return string with HTML tag            fillColor and text
+    const newShape = new Triangle()
+
+}
